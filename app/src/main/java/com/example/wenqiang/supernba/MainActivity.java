a148cbd27575166360f291a3dd6c8585fddabbd3
wenqiang <717092390@qq.com>
2017-12-06 14:34:03 +0800

    集成OKhttp3.9.1,Glide3.7.0

--- app/src/main/java/com/example/wenqiang/supernba/MainActivity.java
+++ app/src/main/java/com/example/wenqiang/supernba/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.wenqiang.supernba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.iv_image);
+
+        Glide.with(this).load("http://07.imgmini.eastday.com/mobile/20170908/20170908171436_d41d8cd98f00b204e9800998ecf8427e_5.jpeg").into(imageView);
     }
 }
